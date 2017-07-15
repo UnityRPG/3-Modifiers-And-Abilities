47f346b322dbea383993a759966d802c856de439
Ben Tristem <github@bentristem.com>
2017-07-15 15:21:29 +0100

    Chasing working

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿using System.Collections;
+﻿﻿﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,93 +8,111 @@ namespace RPG.Characters
     [RequireComponent(typeof(WeaponSystem))]
     public class EnemyAI : MonoBehaviour
     {
-        [SerializeField] float chaseRadius = 6f; // todo impliment?
+        [SerializeField] float chaseRadius = 6f;
         [SerializeField] float deathVanishSeconds = 2.0f;
         [SerializeField] WaypointContainer patrolPath;
         [SerializeField] float waypointTolerance = 2.0f;
         [SerializeField] [Range(0f, 20f)] float minWaitTime = 0f;
         [SerializeField] [Range(0f, 60f)] float maxWaitTime = 2.0f;
 
-		float lastHitTime = 0f;
+        float lastHitTime = 0f;
         WeaponSystem weaponSystem;
         PlayerControl player;
         Character character = null;
         int nextWaypoint = 0;
+        float currentWeaponRange;
+        float distanceToPlayer;
 
-        enum State  {idle, patrolling, attacking, chasing }
-        State state = State.idle;
+        enum State { idle, patrolling, attacking, chasing }
+        [SerializeField] State state = State.idle; //todo private
 
         void Start()
         {
-			weaponSystem = GetComponent<WeaponSystem>();
-            character = GetComponent<Character>();
-            player = FindObjectOfType<PlayerControl>();
-            AttemptToPatrol();
+			character = GetComponent<Character>();
+            print(character);
+			player = FindObjectOfType<PlayerControl>();
         }
 
-        void Update()
+		public float GetDeathVanishDelay()
+		{
+			return deathVanishSeconds;
+		}
+
+		void Update()
         {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-            float currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
 
-			float weaponHitPeriod = weaponSystem.GetCurrentWeapon().GetMinTimeBetweenHits();
-            float timeToWait = weaponHitPeriod * character.GetAnimSpeedMultiplier();
-            bool isTimeToHitAgain = Time.time - lastHitTime > timeToWait;
+           distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+			weaponSystem = GetComponent<WeaponSystem>();
+            currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
 
-			if (distanceToPlayer > currentWeaponRange)
-			{
-				AttemptToPatrol();
-			}
-
-            if (distanceToPlayer < currentWeaponRange)
-            {
-                StopAllCoroutines(); // consider getting handle and being specific
-                state = State.attacking;
-            }
-
-            if (isTimeToHitAgain && state == State.attacking)
+            if (distanceToPlayer > chaseRadius && state != State.patrolling)
             {
                 StopAllCoroutines();
-                weaponSystem.AttackTarget(player.gameObject);
-                lastHitTime = Time.time;
+                StartCoroutine(Patrol());
+            }
+            if (distanceToPlayer <= chaseRadius && state != State.chasing)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ChasePlayer());
+            }
+            if (distanceToPlayer <= currentWeaponRange && state != State.attacking)
+            {
+                StopAllCoroutines();
+				print(gameObject + " Starting attack");
+				StartCoroutine(AttackPlayer());
             }
         }
 
-        void AttemptToPatrol()
+		IEnumerator Patrol()
 		{
-            if (patrolPath != null && state != State.patrolling)
+			state = State.patrolling;
+			while (patrolPath != null) // patrol forever, or at least until childIndex overflows!
 			{
-				state = State.patrolling;
-				StartCoroutine(Patrol(patrolPath));
-			}  
+				character.SetDestination(patrolPath.transform.GetChild(nextWaypoint).position);
+				if (Vector3.Distance(transform.position, patrolPath.transform.GetChild(nextWaypoint).transform.position) <= waypointTolerance)
+				{
+					nextWaypoint = (nextWaypoint + 1) % patrolPath.transform.childCount;
+				}
+				yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+			}
+		}
+
+        IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            while (distanceToPlayer >= currentWeaponRange)
+            { 
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
-        public float GetDeathVanishDelay()
+        IEnumerator AttackPlayer()
         {
-            return deathVanishSeconds;
+			state = State.attacking;
+            while (true)
+            {
+                float weaponHitPeriod = weaponSystem.GetCurrentWeapon().GetMinTimeBetweenHits();
+                float timeToWait = weaponHitPeriod * character.GetAnimSpeedMultiplier();
+				bool isTimeToHitAgain = Time.time - lastHitTime > timeToWait;
+                if (isTimeToHitAgain)
+                {
+                    weaponSystem.AttackTarget(player.gameObject);
+					lastHitTime = Time.time;
+                }
+				yield return new WaitForSeconds(timeToWait);
+            }
         }
 
-	    void OnDrawGizmos()
+        void OnDrawGizmos()
         {
-            // todo show current weapon range
+            // Draw chase sphere 
+            Gizmos.color = new Color(255, 0, 0, .5f);
+            Gizmos.DrawWireSphere(transform.position, currentWeaponRange);
 
             // Draw chase sphere 
             Gizmos.color = new Color(0, 0, 255, .5f);
             Gizmos.DrawWireSphere(transform.position, chaseRadius);
         }
-
-
-        IEnumerator Patrol(WaypointContainer path)
-        {
-            while (true) // patrol forever, or at least until childIndex overflows!
-            {
-                character.SetDestination(path.transform.GetChild(nextWaypoint).position);
-                if (Vector3.Distance(transform.position, path.transform.GetChild(nextWaypoint).transform.position) <= waypointTolerance)
-                {
-                    nextWaypoint = (nextWaypoint + 1) % path.transform.childCount;
-                }
-                yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
-            }
-        }
-	}
+    }
 }
